f687d4ca52da44d533e68ae1335ef0ed41665bba
Colin <colin.diesh@gmail.com>
2025-05-05 16:49:56 -0400

    Add nuqs for url state storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,38 @@
-import { useEffect, useState } from 'react'
-
 import { createJBrowseTheme } from '@jbrowse/core/ui/theme'
 import { ThemeProvider } from '@mui/material/styles'
 import { observer } from 'mobx-react'
+import { useQueryState } from 'nuqs'
 
 import GeneTreeId from './GeneTreeId'
 import Header from './Header'
 import TreeFamId from './TreeFamId'
-import { getUrlParams, updateUrlParams } from './urlParams'
 
 const App = observer(function () {
-  const urlParams = getUrlParams()
-  const [value, setValue] = useState(urlParams.id)
-  const [id, setId] = useState(urlParams.id)
-  const [type, setType] = useState<'treeFam' | 'geneTree'>(urlParams.type)
-
-  useEffect(() => {
-    updateUrlParams(type, id)
-  }, [type, id])
+  // Use nuqs to manage URL state
+  const [id, setId] = useQueryState('id', { defaultValue: '' })
+  const [value, setValue] = useQueryState('value', { defaultValue: id })
+  const [type, setType] = useQueryState('type', {
+    defaultValue: 'treeFam' as 'treeFam' | 'geneTree',
+    parse: value => (value === 'geneTree' ? 'geneTree' : 'treeFam'),
+  })
 
   return (
-    <div>
-      <div className="m-2 p-2">
-        <Header
-          type={type}
-          setType={setType}
-          id={id}
-          setId={setId}
-          val={value}
-          setVal={setValue}
-        />
-        {id ? (
-          type === 'geneTree' ? (
-            <GeneTreeId geneTreeId={id} />
-          ) : (
-            <TreeFamId treeFamId={id} />
-          )
-        ) : null}
-      </div>
+    <div className="m-2 p-2">
+      <Header
+        type={type}
+        setType={setType}
+        id={id}
+        setId={setId}
+        val={value}
+        setVal={setValue}
+      />
+      {id ? (
+        type === 'geneTree' ? (
+          <GeneTreeId geneTreeId={id} />
+        ) : (
+          <TreeFamId treeFamId={id} />
+        )
+      ) : null}
     </div>
   )
 })
